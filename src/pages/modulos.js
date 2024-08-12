132575d7c0ab67a033e66487cb92afc89a9066ad
Derek Paul <derek.paul@brilliant360.net>
2024-08-12 13:46:46 -0500

    done config to modules

---
+++ src/pages/modulos.js
@@ -0,0 +1,12 @@
+import { Separator } from "@/styled-components/layout";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+export default function Modulos() {
+  return (
+    <>
+      <Separator $right />
+    </>
+  );
+}
